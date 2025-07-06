14ddafd671dae8b96ab017cbc53f7b89e9872294
luusytruong <lst27062004@gmail.com>
2025-07-06 20:57:31 +0700

    update

---
+++ src/routes/path.js
@@ -0,0 +1,8 @@
+const path = {
+  root: "/",
+  setting: "/setting",
+  filter: "/filter",
+  delete: "/delete",
+};
+
+export default path;
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,16 +2,18 @@ import Filter from "~/components/Filter/Filter";
 import Setting from "~/components/Setting/Setting";
 import Home from "~/pages/Home/Home";
 import Delete from "~/components/Delete/Delete";
+import path from "./path";
 
 const routes = [
   {
-    path: "/",
+    path: path.root,
     element: <Home />,
     children: [
-      { path: "/setting", element: <Setting /> },
-      { path: "/filter", element: <Filter /> },
-      { path: "/delete", element: <Delete /> },
+      { path: path.setting, element: <Setting /> },
+      { path: path.filter, element: <Filter /> },
+      { path: path.delete, element: <Delete /> },
     ],
   },
 ];
+
 export default routes;
